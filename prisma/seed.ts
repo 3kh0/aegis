9c7d320e5378e7f1b95d0b12f3123d7ade863d1b
Echo <github@3kh0.net>
2026-02-01 17:16:29 -0500

    feat: add default seed program (#9)
    * feat: add default seed program
    
    * linting

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "./generated/client";
+
+const db = new PrismaClient();
+
+const programs = [
+  {
+    slug: "security",
+    title: "Aegis",
+    description: "The very platform you are on!",
+    iconUrl: "https://aegis.3kh0.net/favicon.png",
+    website: "https://aegis.3kh0.net",
+    content: null,
+  },
+];
+
+for (const p of programs) await db.program.upsert({ where: { slug: p.slug }, update: {}, create: p });
+
+console.log("Seeded default programs");
+await db.$disconnect();
